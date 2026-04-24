cc93d61f4baf5e39501ab76a33fb89be02b9fb8b
DESKTOP-RR27EJ8\Hussein <husseinstohy@gmail.com>
2026-04-24 22:11:27 +0300

    feat: add environment configuration files for production and development baseUrl settings

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  baseUrl: 'http://localhost/grad_project'
+  baseUrl: 'https://exciting-appreciation-production-bf69.up.railway.app',
+  railwayId: '6613677f-deb8-4683-81e8-4f6773e01dd6'
 };
